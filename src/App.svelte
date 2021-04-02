50e7c52b7d2caec4f04cbd999cf2f17ae947ac4a
thijsj3 <24222526+thijsj3@users.noreply.github.com>
2021-04-02 17:42:33 +0200

    Added my first content!
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   public/TI_logo.png
    	modified:   public/api/stories/all.json
    	new file:   public/css/style.css
    	modified:   public/index.html
    	modified:   src/App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,13 @@
 <script>
-	import "bootstrap/dist/css/bootstrap.css";
 	import { onMount } from "svelte";
+	import "bootstrap/dist/css/bootstrap.css";
 
 	let stories = [];
 	let currentItems = 1;
 	let amount = 7;
 	let countStories;
-	let title = "A Thijs website"
-	let subtitle = "This isn't even my final form 🐲";
+	let title = "This isn't even my final form 🐲";
+	let coverImage = "https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
 
 	async function getStories() {
 		const res = await fetch("api/stories/all.json");
@@ -23,56 +23,25 @@
 	}
 </script>
 
-<div class="jumbotron jumbotron-fluid">
-	<div class="container">
-		<h1 class="display-4">{title}</h1>
-		<p class="lead">
-			{subtitle}
-		</p>
-	</div>
-</div>
+<img
+	src={coverImage}
+	class="enough-space rounded "
+	alt="header_image by unsplash.com/@halacious"
+/>
+
+<h1 class="display-4 text-center">{title}</h1>
+
+<div />
 
 {#await getStories()}
 	<p>loading</p>
 {:then stories}
-	<div class="card-columns">
 		{#each stories as story}
-			{#if story.image && story.title}
-				<div class="card">
-					<img
-						src={story.image}
-						class="card-img-top"
-						object-fit="cover"
-						width="100%"
-						height="200rem"
-						alt={story.imageAuthor}
-					/>
-					<div class="card-body">
-						<h5 class="card-title">{story.title}</h5>
-						<p class="card-text">{story.content}</p>
-					</div>
-				</div>
-			{:else if story.image}
-				<div class="card">
-					<img
-						src={story.image}
-						class="card-img-top"
-						object-fit="cover"
-						width="100%"
-						height="200rem"
-						alt={story.imageAuthor}
-					/>
-				</div>
-			{:else if story.title}
-				<div class="card">
-					<div class="card-body">
-						<h5 class="card-title">{story.title}</h5>
-						<p class="card-text">{story.content}</p>
-					</div>
-				</div>
-			{/if}
+			<div class="card mb-3 text-center">
+					<h2 class="card-title">{story.title}</h2>
+					<p class="card-text">{story.content}</p>
+			</div>
 		{/each}
-	</div>
 {:catch error}
 	<p style="color: red">error</p>
 {/await}
