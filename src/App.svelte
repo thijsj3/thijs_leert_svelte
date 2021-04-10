1f6e9121d9c79cdd120fc3bf08f95202288b8929
thijsj3 <24222526+thijsj3@users.noreply.github.com>
2021-04-10 14:17:28 +0200

    removed bootstrap added own css

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,43 @@
 <script>
-	import { onMount } from "svelte";
-	import "bootstrap/dist/css/bootstrap.css";
-	import Story from './Story.svelte';
+	import Story from "./Story.svelte";
 
 	let stories = [];
-	let currentItems = 1;
-	let amount = 7;
-	let countStories;
 	let title = "This isn't even my final form 🐲";
-	let coverImage = "https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+	let coverImage =
+		"https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80";
 
 	async function getStories() {
 		const res = await fetch("api/stories/all.json");
 		stories = await res.json();
-		countStories = stories.length;
-		// console.log(count)
 		return stories;
-	}
-
-	function handleClick(amount) {
-		console.log(stories);
-		currentItems = currentItems + amount;
 	}
 </script>
 
+<style>
+	.title {
+		font-size: 3.5rem;
+    	font-weight: 300;
+    	line-height: 1.2;
+		text-align: center !important;
+	}
+</style>
+
 <img
 	src={coverImage}
-	class="enough-space rounded "
+	class="enough-space-for-picture rounded "
 	alt="header_image by unsplash.com/@halacious"
 />
 
-<h1 class="display-4 text-center">{title}</h1>
+<h1 class="title">{title}</h1>
 
 <div />
 
 {#await getStories()}
 	<p>loading</p>
 {:then stories}
-		{#each stories as story}
-			<Story title={story.title} content={story.content}/>
-		{/each}
+	{#each stories as story}
+		<Story title={story.title} content={story.content} />
+	{/each}
 {:catch error}
 	<p style="color: red">error</p>
 {/await}
--- src/Story.svelte
+++ src/Story.svelte
@@ -1,13 +1,27 @@
 <script>
-    import "bootstrap/dist/css/bootstrap.css";
+    import marked from 'marked';
+    import { fade } from 'svelte/transition';
 	export let title;
     export let content;
 </script>
 
-<div class="card" style="width: 18rem;">
-    <div class="card-body">
-        <h2 class="card-title">{title}</h2>
-        <p class="card-text">{content}</p>
+<style>
+	.box {
+        box-sizing: border-box;
+		width: 300px;
+		border: 1px solid #aaa;
+		border-radius: 2px;
+		box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+		padding: 1em;
+		margin: 0 0 1em 0;
+        object-fit: cover;
+        width: 100%;
+	}
+</style>
+
+<div class="box">
+    <div>
+        <h2>{title}</h2>
+        <p transition:fade>{@html marked(content)}</p>
     </div>
-    
 </div>
